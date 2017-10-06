a916a53e9008a6aecb5079c5fee5b66b7bedc711
Nexus Web Development <github@nexwebsites.com>
2017-10-06 05:41:37 -0400

    Update flushable_buffer.go

--- flushable_buffer.go
+++ flushable_buffer.go
@@ -4,16 +4,19 @@ import (
 	"bytes"
 	"io"
 )
-  // FlushableBuffer 
+
+// FlushableBuffer ...
 type FlushableBuffer struct {
 	*bytes.Buffer
 	stream io.Writer
 }
 
+// Flush ...
 func (b *FlushableBuffer) Flush() (n int, err error) {
 	return b.stream.Write(b.Bytes())
 }
 
+// NewFlushableBuffer ...
 func NewFlushableBuffer(stream io.Writer) *FlushableBuffer {
 	return &FlushableBuffer{
 		Buffer: bytes.NewBufferString(""),
